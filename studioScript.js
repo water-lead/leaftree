a5b4922c50569515870c3ab413e032965685d83c
Kenneth <nnabuifekenneth766@gmail.com>
2023-10-27 08:01:40 +0100

    added features

--- studioScript.js
+++ studioScript.js
@@ -1,137 +1,169 @@
+//This displays
 function showIndustryInput1() {
-  document.getElementById("loginButton").style.display = "none";
-  document.getElementById("industryInput1").style.display = "block";
+  document.getElementById("first-studio-display").style.display = "none";
+  document.getElementById("enter-industries").style.display = "block";
+  document.getElementById("industry1").style.display = "block";
+  document.getElementById("industry2").style.display = "none";
   document.getElementById("industry1").focus(); // Sets focus on the username input field
 }
 
-function showIndustryInput2() {
-  document.getElementById("industryInput1").style.display = "none";
-  document.getElementById("industryInput2").style.display = "block";
-  document.getElementById("industry1").focus(); // Sets focus on the password input field
-}
-
-async function submitIndustryForm() {
-  const username = document.getElementById("industry1").value;
-  const password = document.getElementById("industry2").value;
-
-  const requestBody = {
-    industry1: industry1,
-    industry2: industry2,
-  };
-
-}
-  function showIdeaInput1() {
-  document.getElementById("proceedButton").style.display = "none";
-  document.getElementById("ideaInput1").style.display = "block";
-  document.getElementById("idea1").focus(); // Sets focus on the username input field
-}
-
-function showIdeaInput2() {
-  document.getElementById("ideaInput1").style.display = "none";
-  document.getElementById("ideaInput2").style.display = "block";
-  document.getElementById("idea2").focus(); // Sets focus on the password input field
-}
-
-  function showIdeaInput3() {
-  document.getElementById("ideaInput2").style.display = "none";
-  document.getElementById("ideaInput3").style.display = "block";
-  document.getElementById("idea3").focus(); // Sets focus on the username input field
-}
-
-function showIdeaInput4() {
-  document.getElementById("ideaInput3").style.display = "none";
-  document.getElementById("ideaInput4").style.display = "block";
-  document.getElementById("idea4").focus(); // Sets focus on the password input field
-}
-
-  function showIdeaInput5() {
-  document.getElementById("ideaInput4").style.display = "none";
-  document.getElementById("ideaInput5").style.display = "block";
-  document.getElementById("idea5").focus(); // Sets focus on the password input field
-}
-
-async function submitIdeaForm() {
-  const username = document.getElementById("idea1").value;
-  const password = document.getElementById("idea2").value;
-  const username = document.getElementById("idea3").value;
-  const password = document.getElementById("idea4").value;
-  const password = document.getElementById("idea5").value;
-
-  const requestBody = {
-    idea1: idea1,
-    idea2: idea2,
-    idea3: idea3,
-    idea4: idea4,
-    idea5: idea5,
-  };
-
-  function analyzeButton() {
-  document.getElementById("analyzeButton").style.display = "none";
-  }
-}
-
-   try {
-    const response = await fetch("https://sample-login-endpoint/login-route", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(requestBody),
-    });
-
-    const data = await response.json();
-
-    if (data.success) {
-      window.location.href = "studio.html"; // Redirects to desired page on successful login
-    } else {
-      const notification = document.getElementById("notification");
-      notification.style.display = "block";
-      notification.innerText = "Incorrect login details. Please try again.";
-    }
-  } catch (error) {
-    console.error("Error:", error);
-    const notification = document.getElementById("notification");
-    notification.style.display = "block";
-    notification.innerText = "You may not have access, please contact us.";
-  }
+function showIdeaInput1() {
+  document.getElementById("congrats-view").style.display = "none";
+  document.getElementById("init-analyse").style.display = "block";
+  document.getElementById("idea1").style.display = "block";
+  document.getElementById("idea1").focus();
 }
 
 document.addEventListener("keydown", function (event) {
-  if (event.key === "Enter") {
-    if (document.getElementById("loginButton").style.display === "block") {
-      showIndustryInput1(); // If login button is displayed, show username input
-    } else if (
-      document.getElementById("industry1").style.display === "block"
-    ) {
-      showIndustryInput2(); // If username input is displayed, show password input
-    } else if (
+  if (event.key === "ArrowRight") {
+    if (
+      document.getElementById("enter-industries").style.display === "block" &&
       document.getElementById("industry2").style.display === "block"
     ) {
-      submitIndustryForm(); // If password input is displayed, submit the login form
+      document.getElementById("enter-industries").style.display = "none";
+      document.getElementById("congrats-view").style.display = "block";
     }
   }
+});
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowRight") {
+    if (
+      document.getElementById("init-analyse").style.display === "block" &&
+      document.getElementById("idea1").style.display === "none" &&
+      document.getElementById("idea2").style.display === "none"
+    ) {
+      if (document.getElementById("idea3").style.display === "block") {
+        document.getElementById("idea3").style.display = "none";
+        document.getElementById("idea4").style.display = "block";
+      } else if (document.getElementById("idea4").style.display === "block") {
+        document.getElementById("idea4").style.display = "none";
+        document.getElementById("idea5").style.display = "block";
+      }
+    }
+  }
+
   if (event.key === "Enter") {
-    if (document.getElementById("proceedButton").style.display === "block") {
-      showIdeaInput1(); // If login button is displayed, show username input
-    } else if (
-      document.getElementById("idea1").style.display === "block"
+    const industry1Value = document.getElementById("industry1").value;
+    const industry2Value = document.getElementById("industry2").value;
+
+    const idea1 = document.getElementById("idea1").value;
+    const idea2 = document.getElementById("idea2").value;
+    const idea3 = document.getElementById("idea3").value;
+    const idea4 = document.getElementById("idea4").value;
+    const idea5 = document.getElementById("idea5").value;
+
+    if (
+      industry1Value &&
+      document.getElementById("enter-industries").style.display === "block"
     ) {
-      showIdeaInput2(); // If username input is displayed, show password input
-    } else if (
-      document.getElementById("idea2").style.display === "block"
+      document.getElementById("industry1").style.display = "none";
+      document.getElementById("industry2").style.display = "block";
+      document.getElementById("industry2").focus();
+    }
+
+    if (
+      industry2Value &&
+      document.getElementById("enter-industries").style.display === "block"
     ) {
-      showIdeaInput3(); // If username input is displayed, show password input
-    } else if (
-      document.getElementById("idea3").style.display === "block"
+      document.getElementById("enter-industries").style.display = "none";
+      document.getElementById("congrats-view").style.display = "block";
+    }
+
+    //This uses right arrow key to skip an move to next step
+    if (
+      idea1 &&
+      document.getElementById("init-analyse").style.display === "block"
     ) {
-      showIdeaInput4(); // If username input is displayed, show password input
-    } else if (
-      document.getElementById("idea4").style.display === "block"
+      document.getElementById("idea1").style.display = "none";
+      document.getElementById("idea2").style.display = "block";
+      document.getElementById("idea2").focus();
+    }
+
+    if (
+      idea2 &&
+      document.getElementById("init-analyse").style.display === "block"
     ) {
-      showIdeaInput5(); // If username input is displayed, show password input
+      document.getElementById("idea2").style.display = "none";
+      document.getElementById("idea3").style.display = "block";
+    }
+
+    if (
+      idea3 &&
+      document.getElementById("init-analyse").style.display === "block"
+    ) {
+      document.getElementById("idea3").style.display = "none";
+      document.getElementById("idea4").style.display = "block";
+      if (
+        idea4 &&
+        document.getElementById("init-analyse").style.display === "block"
+      ) {
+        document.getElementById("idea4").style.display = "none";
+        document.getElementById("idea5").style.display = "block";
+      }
+    }
+  }
+});
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowLeft") {
+    if (document.getElementById("industry1").style.display === "block") {
+      document.getElementById("industry1").style.display = "none";
+      document.getElementById("first-studio-display").style.display = "block";
+      document.getElementById("enter-industries").style.display = "none";
     } else if (
+      document.getElementById("enter-industries").style.display === "block" &&
+      document.getElementById("industry2").style.display === "block"
+    ) {
+      document.getElementById("industry1").style.display = "block";
+      document.getElementById("industry2").style.display = "none";
+      document.getElementById("industry1").focus();
+      document.getElementById("industry1").style.marginLeft = "25%";
+      document.getElementById("industry1").value = "";
+    } else if (
+      document.getElementById("init-analyse").style.display === "block" &&
       document.getElementById("idea5").style.display === "block"
     ) {
-      submitIdeaForm(); // If password input is displayed, submit the login form
+      document.getElementById("idea5").style.display = "none";
+      document.getElementById("idea4").style.display = "block";
+      document.getElementById("idea4").focus();
+    } else if (
+      document.getElementById("init-analyse").style.display === "block" &&
+      document.getElementById("idea4").style.display === "block"
+    ) {
+      document.getElementById("idea4").style.display = "none";
+      document.getElementById("idea3").style.display = "block";
+      document.getElementById("idea3").focus();
+    } else if (
+      document.getElementById("init-analyse").style.display === "block" &&
+      document.getElementById("idea3").style.display === "block"
+    ) {
+      document.getElementById("idea3").style.display = "none";
+      document.getElementById("idea2").style.display = "block";
+      document.getElementById("idea2").focus();
+    } else if (
+      document.getElementById("init-analyse").style.display === "block" &&
+      document.getElementById("idea2").style.display === "block"
+    ) {
+      document.getElementById("idea2").style.display = "none";
+      document.getElementById("idea1").style.display = "block";
+      document.getElementById("idea1").focus();
+      document.getElementById("idea1").style.marginLeft = "25%";
+      document.getElementById("idea1").value = "";
+    } else if (
+      document.getElementById("init-analyse").style.display === "block" &&
+      document.getElementById("idea1").style.display === "block"
+    ) {
+      document.getElementById("init-analyse").style.display = "none";
+      document.getElementById("congrats-view").style.display = "block";
+    } else if (
+      document.getElementById("congrats-view").style.display === "block" &&
+      document.getElementById("enter-industries").style.display === "none"
+    ) {
+      document.getElementById("congrats-view").style.display = "none";
+      document.getElementById("enter-industries").style.display = "block";
+      document.getElementById("industry2").style.display = "block";
+      document.getElementById("industry1").focus();
     }
+  }
 });
