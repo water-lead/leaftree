d9e82db1a356135f4b18aa048b6c54595c6f0519
water-lead <148636323+water-lead@users.noreply.github.com>
2023-10-30 01:28:56 +0100

    Update studioScript.js

--- studioScript.js
+++ studioScript.js
@@ -2,9 +2,10 @@
 function showIndustryInput1() {
   document.getElementById("first-studio-display").style.display = "none";
   document.getElementById("enter-industries").style.display = "block";
+  /*document.getElementById("analyse-view").style.display = "none";*/
   document.getElementById("industry1").style.display = "block";
   document.getElementById("industry2").style.display = "none";
-  document.getElementById("industry1").focus();
+  document.getElementById("industry1").focus(); // Sets focus on the username input field
 }
 
 function showIdeaInput1() {
@@ -60,23 +61,103 @@ function runEngine() {
 document.addEventListener("keydown", function (event) {
   if (event.key === "ArrowRight") {
     if (
+      document.getElementById("enter-industries").style.display === "block" &&
+      document.getElementById("industry2").style.display === "block"
+    ) {
+      document.getElementById("enter-industries").style.display = "none";
+      document.getElementById("congrats-view").style.display = "block";
+      document.getElementById("analyse-view").style.display = "none";
+    }
+  }
+});
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowRight") {
+    if (
       document.getElementById("init-analyse").style.display === "block" &&
       document.getElementById("idea1").style.display === "none" &&
       document.getElementById("idea2").style.display === "none"
     ) {
-        if (document.getElementById("idea3").style.display === "block") {
-            document.getElementById("idea3").style.display = "none";
-            document.getElementById("idea4").style.display = "block";
-            document.getElementById("analyse-view").style.display = "none";
-        } else if (document.getElementById("idea4").style.display === "block") {
-            document.getElementById("idea4").style.display = "none";
-            document.getElementById("idea5").style.display = "block";
-            document.getElementById("analyse-view").style.display = "none";
-        } else if (document.getElementById("idea5").style.display === "block") {
-            document.getElementById("idea5").style.display = "none";
-            document.getElementById("analyse-view").style.display = "block";
-        }
-    };
+      if (document.getElementById("idea3").style.display === "block") {
+        document.getElementById("idea3").style.display = "none";
+        document.getElementById("idea4").style.display = "none";
+      } else if (document.getElementById("idea4").style.display === "block") {
+        document.getElementById("idea4").style.display = "none";
+        document.getElementById("idea5").style.display = "block";
+        document.getElementById("analyse-view").style.display = "none";
+      } else if (document.getElementById("idea5").style.display === "block") {
+        document.getElementById("idea5").style.display = "none";
+        document.getElementById("analyse-view").style.display = "none";
+      } 
+    }
+  }
+
+  if (event.key === "Enter") {
+    const industry1Value = document.getElementById("industry1").value;
+    const industry2Value = document.getElementById("industry2").value;
+
+    const idea1 = document.getElementById("idea1").value;
+    const idea2 = document.getElementById("idea2").value;
+    const idea3 = document.getElementById("idea3").value;
+    const idea4 = document.getElementById("idea4").value;
+    const idea5 = document.getElementById("idea5").value;
+
+    if (
+      industry1Value &&
+      document.getElementById("enter-industries").style.display === "block"
+    ) {
+      document.getElementById("industry1").style.display = "none";
+      document.getElementById("industry2").style.display = "block";
+      document.getElementById("industry2").focus();
+    }
+
+    if (
+      industry2Value &&
+      document.getElementById("enter-industries").style.display === "block"
+    ) {
+      document.getElementById("enter-industries").style.display = "none";
+      document.getElementById("congrats-view").style.display = "block";
+      document.getElementById("analyse-view").style.display = "none";
+    }
+
+    //This uses right arrow key to skip an move to next step
+    if (
+      idea1 &&
+      document.getElementById("init-analyse").style.display === "block"
+    ) {
+      document.getElementById("idea1").style.display = "none";
+      document.getElementById("idea2").style.display = "block";
+      document.getElementById("analyse-view").style.display = "none";
+      document.getElementById("idea2").focus();
+    }
+
+    if (
+      idea2 &&
+      document.getElementById("init-analyse").style.display === "block"
+    ) {
+      document.getElementById("idea2").style.display = "none";
+      document.getElementById("idea3").style.display = "block";
+      document.getElementById("analyse-view").style.display = "none";
+    }
+
+    if (
+      idea3 &&
+      document.getElementById("init-analyse").style.display === "block"
+    ) {
+      document.getElementById("idea3").style.display = "none";
+      document.getElementById("idea4").style.display = "none";
+      document.getElementById("analyse-view").style.display = "block";
+      if (
+        idea4 &&
+        document.getElementById("init-analyse").style.display === "block"
+      ) {
+        document.getElementById("idea4").style.display = "none";
+        document.getElementById("idea5").style.display = "none";
+        /*document.getElementById("analyse-view").style.display = "block";*/
+      }
+    }
+  }
+});
 
 document.addEventListener("keydown", function (event) {
   if (event.key === "ArrowLeft") {
