ef2151d3e3f00d0f10017fce50471fa20dffae3f
water-lead <148636323+water-lead@users.noreply.github.com>
2024-03-24 15:08:13 +0100

    Update principlesScript.js

--- principlesScript.js
+++ principlesScript.js
@@ -1,8 +1,9 @@
 //This displays
 function acceptEmail() {
   document.getElementById("principles-Display").style.display = "none";
-  document.getElementById("form-submission").style.display = "block";
-  document.getElementById("emailButton").style.display = "none";
+  document.getElementById(""enter-information").style.display = "block";
+  document.getElementById("name").style.display = "block";
+  document.getElementById("email").style.display = "none";
   document.getElementById("name").focus(); // Sets focus on the username input field
 }
 
