aad9466d85c1199232b141f1a58b54eae94d6057
water-lead <148636323+water-lead@users.noreply.github.com>
2024-03-24 17:17:35 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+// tailwind.config.js
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: '#ac5989',
+      },
+      borderWidth: {
+        '3': '3px',
+      },
+      borderRadius: {
+        'xl': '1rem',
+      },
+      padding: {
+        '150px': '150px',
+      },
+      margin: {
+        '10vw': '10vw',
+      },
+      maxWidth: {
+        '400px': '400px',
+        '600px': '600px',
+      },
+      fontSize: {
+        '20px': '20px',
+        '25px': '25px',
+      },
+      screens: {
+        'mobile-background-video': '540px',
+        'tablet-background-video': '640px',
+        'laptop-background-video': '1024px',
+        'desktop-background-video': '1280px',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
+
