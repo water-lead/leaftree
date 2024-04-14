13d804896ade9e8801b004abda8aa2d52b588292
water-lead <148636323+water-lead@users.noreply.github.com>
2024-04-14 12:18:03 +0100

    Update Principle.jsx

--- src/containers/Principle.jsx
+++ src/containers/Principle.jsx
@@ -81,16 +81,15 @@ function Form3({referral, setReferral, Payload}) {
         onChange={(e) => handleInputChange(e)}
     /> 
     </form>
-   const scriptUrl => {"https://script.google.com/a/macros/waterdesigns.io/s/AKfycbxWuSPhgJ8uMKCjDPzQafAQADM2kNUR6pvUULGb9_acYdfvuVI6hnv62woohGAHogQ_1Q/exec"}
-   const handleSubmit = (e) => {
+   const scriptUrl = "https://script.google.com/a/macros/waterdesigns.io/s/AKfycbxWuSPhgJ8uMKCjDPzQafAQADM2kNUR6pvUULGb9_acYdfvuVI6hnv62woohGAHogQ_1Q/exec";
+const handleSubmit = (e) => {
     e.preventDefault();
     fetch(scriptUrl, { method: 'POST', body: new FormData(formRef.current) })
         .then(res => {
             console.log("SUCCESSFULLY SUBMITTED");
         })
         .catch(err => console.log(err));
-    };
-
+};
     return (
     <input 
         type="text" 
