b55e0c49ef54aed03a94dd2448ea2a7859cefb43
water-lead <148636323+water-lead@users.noreply.github.com>
2024-04-14 16:26:29 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ function App() {
   return (
     <>
       <Routes>
-       <Route exact path="/" element={<Home />} />
-       <Route exact path="/About Us" element={<About />} />
-       <Route exact path="/Our Principles" element={<Principle />} />
+       <Route path="/" element={<Home />} />
+       <Route path="/About Us" element={<About />} />
+       <Route path="/Our Principles" element={<Principle />} />
       </Routes>
     </>
   )
