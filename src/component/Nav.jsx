d5e202ad50f644e9afeee7c30ecfc116f4a87949
water-lead <148636323+water-lead@users.noreply.github.com>
2024-04-21 23:07:34 +0100

    Update Nav.jsx

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -1,31 +1,30 @@
-// import React from "react"
-import '../App.css'
-import Logo from '../assets/Logo.gif'
-import { Link } from "react-router-dom"
-import Menu from '../assets/icons8-menu-64.png'; // Import the menu icon from react-icons/fa
-import NextScreen from '../assets/icons8-move-right-100.png'; // Import the right arrow icon from react-icons/md
+import '../App.css';
+import Logo from '../assets/Logo.gif';
+import { Link } from "react-router-dom";
+import MenuIcon from '../assets/icons8-menu-64.png'; // Import the menu icon image
+import NextScreen from '../assets/icons8-move-right-100.png'; // Import the right arrow icon image
 
 export default function Nav() {
  return (
    <>
     <nav className="menu">
-     <li>
-        <Link to="/">
-           <img src={Logo} alt="Logo" className="logo" />
-        </Link>
-      </li>
-      <div className="menu-items">
-          <img src="{Menu}" alt="Menu" className="menu" /> {/* Use your custom menu icon */}
-  </div>
-  <div className="menu-items">
-        <Link to="/how we invest" className='menu-items-link'>How</Link>
-        <Link to="/what we believe" className='menu-items-link'>What</Link>
-        <Link to="/who we are" className='menu-items-link'>Who</Link>
-        <Link to="/jobs" className='menu-items-link'>Jobs</Link>
-  </div>
-</div> 
-      </div>
+      <ul className="menu-items">
+        <li>
+          <Link to="/">
+            <img src={Logo} alt="Logo" className="logo" />
+          </Link>
+        </li>
+        <li>
+          <img src={MenuIcon} alt="Menu" className="menu-icon" /> {/* Use your custom menu icon */}
+        </li>
+        <li>
+          <Link to="/how-we-invest" className='menu-items-link'>How</Link>
+          <Link to="/what-we-believe" className='menu-items-link'>What</Link>
+          <Link to="/who-we-are" className='menu-items-link'>Who</Link>
+          <Link to="/jobs" className='menu-items-link'>Jobs</Link>
+        </li>
+      </ul>
     </nav>
-  </>
+  <>
  )
 }
