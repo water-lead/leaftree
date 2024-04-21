5a38ee0569bb87c17aa139d727bb376e26f6b7dc
water-lead <148636323+water-lead@users.noreply.github.com>
2024-04-21 22:12:54 +0100

    Update and rename Jobs.html to Jobs.jsx

---
+++ src/containers/Jobs.jsx
@@ -0,0 +1,15 @@
+// Jobs.jsx
+import React from "react";
+
+function Jobs() {
+  return (
+    <div>
+      <h1>Jobs Page</h1>
+      {
+      <div style="position: relative; width: 100%; height: 0; padding-top: 56.2225%; padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden; border-radius: 8px; will-change: transform;">  <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"    src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGDDCN0PJE&#x2F;4a8nwJ3HBjQNbM65hXXOEQ&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">  </iframe></div><a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGDDCN0PJE&#x2F;4a8nwJ3HBjQNbM65hXXOEQ&#x2F;view?utm_content=DAGDDCN0PJE&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">TRUTH</a> by Bomi Mosuro
+      }
+    </div>
+  );
+}
+
+export default Jobs;
