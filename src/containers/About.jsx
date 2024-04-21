ea4b2b92f4c915101f4f74a9a3652c3c5b3d91a6
water-lead <148636323+water-lead@users.noreply.github.com>
2024-04-21 20:38:47 +0100

    Update About.jsx

--- src/containers/About.jsx
+++ src/containers/About.jsx
@@ -1,7 +1,7 @@
 // import React from "react"
 import Nav from "../component/Nav.jsx"
-import Widescreen from "../assets/WidescreenAU.mp4"
-import MobileScreen from "../assets/WhatsApp Video 2024-04-11 at 20.45.02.mp4"
+import Widescreen from "../assets/WidescreenAU.png"
+//import MobileScreen from "../assets/WhatsApp Video 2024-04-11 at 20.45.02.mp4"
 import "../App.css"
 
 function determineSrc(type) {
@@ -29,10 +29,10 @@ export default function About() {
       {GetScreenWidth <= 480 && <div id="blur">
         <p>The site can only be viewed in portrait mode. Please tilt your phone</p>
       </div>}
-      <div id="HomescreeVideoContainer">
-        <video id="HomescreenVideo" autoPlay muted loop>
-            <source src={determineSrc(determineParams())} type="video/mp4" />
-        </video>
+      <div id="HomescreenBackgroundContainer">
+        <img id="HomescreenBackground">
+            <source src={determineSrc(determineParams())} type="img/png" />
+        </img>
       </div>
     </>
   )
