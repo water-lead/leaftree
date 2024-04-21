206709f5b9cd0c6d4b32289026d48f42cd5975e1
water-lead <148636323+water-lead@users.noreply.github.com>
2024-04-21 23:36:00 +0100

    Update Jobs.jsx

--- src/containers/Jobs.jsx
+++ src/containers/Jobs.jsx
@@ -4,8 +4,7 @@ import React from "react";
 function Jobs() {
   return (
     <div>
-      <h1>Jobs Page</h1>
-      <div style={{ position: "relative", width: "100%", height: 0, paddingTop: "56.2225%", paddingBottom: 0, boxShadow: "0 2px 8px 0 rgba(63,69,81,0.16)", marginTop: "1.6em", marginBottom: "0.9em", overflow: "hidden", borderRadius: "8px", willChange: "transform" }}>
+      <div style={{ position: "center", width: "100%", height: 0, paddingTop: "56.2225%", paddingBottom: 0, boxShadow: "0 2px 8px 0 rgba(63,69,81,0.16)", marginTop: "1.6em", marginBottom: "0.9em", overflow: "hidden", borderRadius: "8px", willChange: "transform" }}>
         <iframe title="Canva Embed" loading="lazy" style={{ position: "absolute", width: "100%", height: "100%", top: 0, left: 0, border: "none", padding: 0, margin: 0 }} src="https://www.canva.com/design/DAGDDCN0PJE/4a8nwJ3HBjQNbM65hXXOEQ/view?embed" allowFullScreen={true}></iframe>
       </div>
       <p>
