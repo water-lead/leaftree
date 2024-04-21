cac660d55a1b0a6912f066e8802164a13fde9136
water-lead <148636323+water-lead@users.noreply.github.com>
2024-04-21 22:15:51 +0100

    Update Jobs.jsx

--- src/containers/Jobs.jsx
+++ src/containers/Jobs.jsx
@@ -5,9 +5,12 @@ function Jobs() {
   return (
     <div>
       <h1>Jobs Page</h1>
-      {
-      <div style="position: relative; width: 100%; height: 0; padding-top: 56.2225%; padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden; border-radius: 8px; will-change: transform;">  <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"    src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGDDCN0PJE&#x2F;4a8nwJ3HBjQNbM65hXXOEQ&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">  </iframe></div><a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGDDCN0PJE&#x2F;4a8nwJ3HBjQNbM65hXXOEQ&#x2F;view?utm_content=DAGDDCN0PJE&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">TRUTH</a> by Bomi Mosuro
-      }
+      <div style={{ position: "relative", width: "100%", height: 0, paddingTop: "56.2225%", paddingBottom: 0, boxShadow: "0 2px 8px 0 rgba(63,69,81,0.16)", marginTop: "1.6em", marginBottom: "0.9em", overflow: "hidden", borderRadius: "8px", willChange: "transform" }}>
+        <iframe title="Canva Embed" loading="lazy" style={{ position: "absolute", width: "100%", height: "100%", top: 0, left: 0, border: "none", padding: 0, margin: 0 }} src="https://www.canva.com/design/DAGDDCN0PJE/4a8nwJ3HBjQNbM65hXXOEQ/view?embed" allowFullScreen={true}></iframe>
+      </div>
+      <p>
+        <a href="https://www.canva.com/design/DAGDDCN0PJE/4a8nwJ3HBjQNbM65hXXOEQ/view?utm_content=DAGDDCN0PJE&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">TRUTH</a> by Bomi Mosuro
+      </p>
     </div>
   );
 }
