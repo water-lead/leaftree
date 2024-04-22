443328f0cdcb9ce28befb627b74f57df815c9063
water-lead <148636323+water-lead@users.noreply.github.com>
2024-04-22 11:42:42 +0100

    Update What.jsx

--- src/containers/What.jsx
+++ src/containers/What.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Nav from "../component/Nav.jsx";
+//import Nav from "../component/Nav.jsx";
 import "../App.css";
 import { useState } from "react";
 import { Link } from "react-router-dom";
