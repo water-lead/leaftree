eb86d811b4fe42ccfd23313df076a0d1810b6aa0
water-lead <148636323+water-lead@users.noreply.github.com>
2024-04-22 12:18:02 +0100

    Update Home.jsx

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,59 +1,24 @@
+import React from "react";
+//import Nav from "../component/Nav.jsx";
+import "../App.css";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import Nav from "../component/Nav.jsx"; // Make sure to import Nav if needed
-import widescreenSrc from "../assets/WidescreenLT.mp4";
-import NextScreen from '../assets/nextPage.gif';
-import "../App.css";
 
-function determineSrc(type) {
+function Home() {
   return (
-    type === "w" ? widescreenSrc :
-    type === "t" ? TabletscreenSrc : // Import TabletscreenSrc if needed
-    type === "l" ? LaptopscreenSrc : // Import LaptopscreenSrc if needed
-    type === "m" ? MobilescreenSrc : // Import MobilescreenSrc if needed
-    ""
-  );
-}
-
-export default function Home() {
-  const GetScreenWidth = window.innerWidth;
-
-  const determineParams = () => {
-    if (GetScreenWidth <= 480) {
-      return "m";
-    } else if (GetScreenWidth <= 768) {
-      return "t";
-    } else if (GetScreenWidth <= 900) {
-      return "l";
-    } else if (GetScreenWidth <= 1200) {
-      return "w";
-    } else {
-      return "w";
-    }
-  };
-  
- return (
-    <>
-      <Nav />
-      {GetScreenWidth <= 480 && (
-        <div id="blur">
-          <p>The site can only be viewed in portrait mode. Please tilt your phone</p>
+    <div>
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", backgroundColor: "#ffffff" }}>
+        <div style={{ width: "auto", height: "auto" }}>
+          <div style={{ position: "relative", width: "100%", height: 0, paddingTop: "56.2225%", paddingBottom: 0, boxShadow: "0 2px 8px 0 rgba(63,69,81,0.16)", overflow: "hidden", borderRadius: "8px", willChange: "transform" }}>
+            <iframe title="Canva Embed" loading="lazy" style={{ position: "absolute", width: "100%", height: "100%", top: 0, left: 0, border: "none", padding: 0, margin: 0 }} src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGDIsgHjVs&#x2F;qJoLoTwrtzbXwrvXSHtqtQ&#x2F;view?embed" allowfullscreen={true}></iframe>
+          </div>
         </div>
-      )}
-      <div id="HomescreenBackgroundContainer">
-        <picture>
-          <img
-            id="HomescreenBackground"
-            src={determineSrc(determineParams())}
-            alt="Background"
-          />
-        </picture>
-        <button onClick={Link} className="navigation-arrow">
-          <Link to="/how we invest" className="navigation-arrow">
-            <img src={NextScreen} alt="Next" />
-          </Link>
-        </button>
+        <p>
+        <a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGDIsgHjVs&#x2F;qJoLoTwrtzbXwrvXSHtqtQ&#x2F;view?utm_content=DAGDIsgHjVs&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener"></a>
+      </p>
       </div>
-    </>
+    </div>
   );
 }
+
+export default Home;
