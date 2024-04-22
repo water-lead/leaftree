7618297d1b62bb99a8237fccc95f65389195e178
water-lead <148636323+water-lead@users.noreply.github.com>
2024-04-22 18:17:37 +0100

    Update What.jsx

--- src/containers/What.jsx
+++ src/containers/What.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import "../App.css";
 
 function What() {
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
