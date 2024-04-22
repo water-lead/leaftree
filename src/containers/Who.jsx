ae063903bddca51e468ef6d508ee90939c0fe4b3
water-lead <148636323+water-lead@users.noreply.github.com>
2024-04-22 23:11:10 +0100

    Update Who.jsx

--- src/containers/Who.jsx
+++ src/containers/Who.jsx
@@ -32,16 +32,14 @@ function Who() {
       </div>
       <p>
         <a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGDIuk7o4o&#x2F;V6fmSz0MEnBW_Lb1S2sJmg&#x2F;view?utm_content=DAGDIuk7o4o&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener"></a>
-      </p>
-    </div>
-       </div>
+     </p>
+      </div>
       {GetScreenWidth <= 1200 && (
         <div id="blur">
           <p>The site can only be viewed in landscape mode. Please tilt your phone</p>
         </div>
-    </div>
       )}
-    </>
+    </div>
   );
 }
 
