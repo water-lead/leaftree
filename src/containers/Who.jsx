6165a3bcb58ea947a518fa3bb49731764507b167
water-lead <148636323+water-lead@users.noreply.github.com>
2024-04-22 16:50:47 +0100

    Update Who.jsx

--- src/containers/Who.jsx
+++ src/containers/Who.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 //import Nav from "../component/Nav.jsx";
+// Import other image sources if needed
 import "../App.css";
 import { useState } from "react";
 import { Link } from "react-router-dom";
@@ -7,17 +8,17 @@ import { Link } from "react-router-dom";
 function Who() {
   return (
     <div>
-      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", backgroundColor: "#ffffff", boxShadow: "none" }}>
-        <div style={{ width: "100%", height: "80%" }}>
-          <div style={{ position: "relative", width: "100%", height: 0, paddingTop: "56.2225%", paddingBottom: 0, boxShadow: "none", overflow: "hidden", borderRadius: "8px", willChange: "transform" }}>
-            <iframe title="Canva Embed" loading="lazy" style={{ position: "absolute", width: "100%", height: "100%", top: 0, left: 0, border: "none", padding: 0, margin: 0 }} src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGDIuk7o4o&#x2F;V6fmSz0MEnBW_Lb1S2sJmg&#x2F;view?embed" allowfullscreen={true}></iframe>
-          </div>
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", backgroundColor: "#ffffff"}}>
+      <div style={{ width: "100%", height: "100%" }}>
+        <div style={{ position: "relative", width: "100%", height: 0, paddingTop: "56.2225%", paddingBottom: 0, overflow: "hidden", borderRadius: "8px", willChange: "transform" }}>
+          <iframe title="Canva Embed" loading="lazy" style={{ position: "absolute", width: "100%", height: "100%", top: 0, left: 0, border: "none", padding: 0, margin: 0 }} src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGDIuk7o4o&#x2F;V6fmSz0MEnBW_Lb1S2sJmg&#x2F;view?embed" allowfullscreen={false}></iframe>
         </div>
-        <p>
+      </div>
+      <p>
         <a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGDIuk7o4o&#x2F;V6fmSz0MEnBW_Lb1S2sJmg&#x2F;view?utm_content=DAGDIuk7o4o&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener"></a>
       </p>
-      </div>
     </div>
+      </div>
   );
 }
 
