e2b886ddfa3b7065e428855171bc872e9eff8c18
water-lead <148636323+water-lead@users.noreply.github.com>
2024-04-22 16:15:56 +0100

    Update How.jsx

--- src/containers/How.jsx
+++ src/containers/How.jsx
@@ -9,7 +9,7 @@ function How() {
       <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", backgroundColor: "#ffffff" }}>
         <div style={{ width: "100%", height: "100%" }}>
           <div style={{ position: "relative", width: "100%", height: 0, paddingTop: "56.2225%", paddingBottom: 0, boxShadow: "0 2px 8px 0 rgba(63,69,81,0.16)", overflow: "hidden", borderRadius: "8px", willChange: "transform" }}>
-            <iframe title="Canva Embed" loading="lazy" style={{ position: "absolute", width: "100%", height: "100%", top: 0, left: 0, border: "none", padding: 0, margin: 0 }} src="https://www.canva.com/design/DAGDIlikyuo/OEI5K6nERPRJWRuAocipOw/view?embed" allowFullScreen={true}></iframe>
+            <iframe title="Canva Embed" loading="lazy" style={{ position: "absolute", width: "100%", height: "100%", top: 0, left: 0, border: "none", padding: 0, margin: 0 }} src="https://www.canva.com/design/DAGDIlikyuo/OEI5K6nERPRJWRuAocipOw/view?embed" allowFullScreen={false}></iframe>
           </div>
         </div>
         <p>
