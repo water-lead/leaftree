a146a7532d75d7ac8d60f967ddec45ef8b172bb1
water-lead <148636323+water-lead@users.noreply.github.com>
2024-04-22 12:19:49 +0100

    Update Home.jsx

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
   return (
     <div>
       <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", backgroundColor: "#ffffff" }}>
-        <div style={{ width: "auto", height: "auto" }}>
+        <div style={{ width: "100%", height: "80%" }}>
           <div style={{ position: "relative", width: "100%", height: 0, paddingTop: "56.2225%", paddingBottom: 0, boxShadow: "0 2px 8px 0 rgba(63,69,81,0.16)", overflow: "hidden", borderRadius: "8px", willChange: "transform" }}>
             <iframe title="Canva Embed" loading="lazy" style={{ position: "absolute", width: "100%", height: "100%", top: 0, left: 0, border: "none", padding: 0, margin: 0 }} src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGDIsgHjVs&#x2F;qJoLoTwrtzbXwrvXSHtqtQ&#x2F;view?embed" allowfullscreen={true}></iframe>
           </div>
