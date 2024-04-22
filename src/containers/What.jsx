633dc1a251c706bf457b8b586844897e63c29db1
water-lead <148636323+water-lead@users.noreply.github.com>
2024-04-22 22:12:54 +0100

    Update What.jsx

--- src/containers/What.jsx
+++ src/containers/What.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+//import Nav from "../component/Nav.jsx";
 import "../App.css";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function What() {
-const GetScreenWidth = window.innerWidth;
+  const GetScreenWidth = window.innerWidth;
 
   const determineParams = () => {
     if (GetScreenWidth <= 480) {
@@ -19,50 +20,13 @@ const GetScreenWidth = window.innerWidth;
       return "w";
     }
   };
-
+  
   return (
     <div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-          backgroundColor: "#ffffff",
-        }}
-      >
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", backgroundColor: "#ffffff" }}>
         <div style={{ width: "100%", height: "100%" }}>
-          <div
-            style={{
-              position: "relative",
-              width: "100%",
-              height: 0,
-              paddingTop: "56.2225%",
-              paddingBottom: 0,
-              overflow: "hidden",
-              borderRadius: "8px",
-            }}
-          >
-            <iframe
-              seamless="seamless"
-              scrolling="no"
-              frameborder="0"
-              title="Canva Embed"
-              loading="lazy"
-              style={{
-                position: "absolute",
-                width: "100%",
-                height: "100%",
-                top: 0,
-                left: 0,
-                border: "none",
-                padding: 0,
-                margin: 0,
-                boxShadow: "none",
-              }}
-              src="https://www.canva.com/design/DAGDIlikyuo/OEI5K6nERPRJWRuAocipOw/view?embed"
-              allowFullScreen={false}
-            ></iframe>
+          <div style={{ position: "relative", width: "100%", height: 0, paddingTop: "56.2225%", paddingBottom: 0, overflow: "hidden", borderRadius: "8px", willChange: "transform" }}>
+            <iframe seamless="seamless" scrolling="no" frameborder="0" title="Canva Embed" loading="lazy" style={{ position: "absolute", width: "100%", height: "100%", top: 0, left: 0, border: "none", padding: 0, margin: 0 }} src="https://www.canva.com/design/DAGDIlikyuo/OEI5K6nERPRJWRuAocipOw/view?embed" allowFullScreen={false}></iframe>
           </div>
         </div>
         <p>
