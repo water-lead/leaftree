4cbdd8d34ada78e7f189486e6e6b3e05c54fc0e0
water-lead <148636323+water-lead@users.noreply.github.com>
2024-04-22 16:43:20 +0100

    Update Jobs.jsx

--- src/containers/Jobs.jsx
+++ src/containers/Jobs.jsx
@@ -11,7 +11,7 @@ function Jobs() {
     <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", backgroundColor: "#ffffff", boxShadow: "none" }}>
       <div style={{ width: "100%", height: "100%" }}>
         <div style={{ position: "relative", width: "100%", height: 0, paddingTop: "56.2225%", paddingBottom: 0, overflow: "hidden", borderRadius: "8px", willChange: "transform" }}>
-          <iframe title="Canva Embed" loading="lazy" style={{ position: "absolute", width: "100%", height: "100%", top: 0, left: 0, border: "none", padding: 0, margin: 0 }} src="https://www.canva.com/design/DAGDDCN0PJE/4a8nwJ3HBjQNbM65hXXOEQ/view?embed" allowFullScreen={true}></iframe>
+          <iframe title="Canva Embed" loading="lazy" style={{ position: "absolute", width: "100%", height: "100%", top: 0, left: 0, border: "none", padding: 0, margin: 0 }} src="https://www.canva.com/design/DAGDDCN0PJE/4a8nwJ3HBjQNbM65hXXOEQ/view?embed" allowFullScreen={false} boxShadow="none"></iframe>
         </div>
       </div>
       <p>
