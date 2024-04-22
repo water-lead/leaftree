8626b09179642edde3f3fc5d9dbef422d7aaec32
water-lead <148636323+water-lead@users.noreply.github.com>
2024-04-22 23:04:32 +0100

    Update How.jsx

--- src/containers/How.jsx
+++ src/containers/How.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function How() {
-const GetScreenWidth = window.innerWidth;
+  const GetScreenWidth = window.innerWidth;
 
   const determineParams = () => {
     if (GetScreenWidth <= 480) {
@@ -19,25 +19,25 @@ const GetScreenWidth = window.innerWidth;
       return "w";
     }
   };
-  
+
   return (
     <div>
       <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", backgroundColor: "#ffffff" }}>
         <div style={{ width: "100%", height: "100%" }}>
           <div style={{ position: "relative", width: "100%", height: 0, paddingTop: "56.2225%", paddingBottom: 0, overflow: "hidden", borderRadius: "8px", willChange: "transform"}}>
-            <iframe seamless="seamless" scrolling="no" frameborder="0" title="Canva Embed" loading="lazy" style={{ position: "absolute", width: "100%", height: "100%", top: 0, left: 0, border: "none", padding: 0, margin: 0 }} src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGDIkjEZ8A&#x2F;XpptsF2iV8gpK2GXLkYGfQ&#x2F;view?embed" allowfullscreen={false} ></iframe>
+            <iframe seamless="seamless" scrolling="no" frameborder="0" title="Canva Embed" loading="lazy" style={{ position: "absolute", width: "100%", height: "100%", top: 0, left: 0, controls: "none", border: "none", padding: 0, margin: 0 }} src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGDIkjEZ8A&#x2F;XpptsF2iV8gpK2GXLkYGfQ&#x2F;view?embed" allowfullscreen={false} ></iframe>
           </div>
         </div>
         <p>
-         <a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGDIkjEZ8A&#x2F;XpptsF2iV8gpK2GXLkYGfQ&#x2F;view?utm_content=DAGDIkjEZ8A&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener"></a>
-      </p>
+          <a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGDIkjEZ8A&#x2F;XpptsF2iV8gpK2GXLkYGfQ&#x2F;view?utm_content=DAGDIkjEZ8A&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener"></a>
+        </p>
       </div>
-    </div>
-    {GetScreenWidth <= 1200 && (
+      {GetScreenWidth <= 1200 && (
         <div id="blur">
           <p>The site can only be viewed in landscape mode. Please tilt your phone</p>
         </div>
       )}
+    </div>
   );
 }
 
